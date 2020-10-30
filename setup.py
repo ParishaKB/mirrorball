c07a6441fbf4d3c51e37ff00a4f084679d656bdb
ParishaKB <43709847+ParishaKB@users.noreply.github.com>
2020-10-30 14:26:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 setup(name='mirrorball',
       version='0.1.7',
       description='A python package to use neural networks to combine the content of an image with the style of another image',
-      url='https://github.com/ParishaKB/mirrorball,
+      url='https://github.com/ParishaKB/mirrorball',
       author='Parisha Bhatia, Soham Sharangpani, Shreyansh Bardia, Ujwal Shah,Aniket Modi,Gaurav Ankalagi',
       license='MIT',
       include_package_data=True,
